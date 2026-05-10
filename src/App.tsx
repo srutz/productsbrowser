006cbaf6340099c9fb38f41476a148cf7c01610e
stepan.rutz <stepan.rutz@gmx.de>
2026-05-10 10:19:38 +0200

    empty project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-stretch justify-center bg-red-300 p-4">
+      <div className="h-1 grow bg-blue-300">
+
+      </div>
+    </div>
+  )
+}
