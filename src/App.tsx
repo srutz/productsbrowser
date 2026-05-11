3aa5e323fe08fffcd6f5609dcfc6973957cd9646
stepan.rutz <stepan.rutz@gmx.de>
2026-05-11 15:06:19 +0200

    mybutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-
+import type { ComponentProps, ReactNode } from "react";
+import { cn } from "./lib/utils";
 
 export function App() {
   return (
-    <div className="w-screen h-screen flex flex-col items-stretch justify-center bg-red-300 p-4">
-      <div className="h-1 grow bg-blue-300">
-
-      </div>
+    <div
+      className="w-screen h-screen flex 
+    flex-col items-stretch justify-center p-4 gap-2"
+    >
+      Window size {window.innerWidth} x {window.innerHeight}
+      <MyButton className=" ">wdwqd</MyButton>
     </div>
-  )
+  );
+}
+
+function MyButton(
+  props: {
+    children: ReactNode;
+  } & ComponentProps<"button">,
+) {
+  const { children, className, ...rest } = props;
+  return (
+    <button
+      {...rest}
+      className={cn(
+        "inline-flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white transition",
+        "hover:bg-blue-700",
+        "shadow-xl",
+        "focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+
+
+const rootElement = document.getElementById("root") as HTMLElement;
+const reactRoot = createRoot(rootElement);
+reactRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
