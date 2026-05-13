e241a037052eccf8426938e3e0b1abd21063d407
stepan.rutz <stepan.rutz@gmx.de>
2026-05-13 11:26:02 +0200

    clean project

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,4 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { useLocalStorage } from "@uidotdev/usehooks"
+import { MyButton } from "../MyButton";
+import { atom, useAtom } from "jotai";
+
+const counterAtom = atom(1)
 
 export const Route = createFileRoute("/about")({
   component: RouteComponent,
@@ -6,5 +12,10 @@ export const Route = createFileRoute("/about")({
 
 // eslint-disable-next-line react-refresh/only-export-components
 function RouteComponent() {
-  return <div>Hello "/about"!</div>;
+  return (
+    <div className="flex gap-2">
+    </div>
+  );
 }
+
+
