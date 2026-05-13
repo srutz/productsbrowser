91badc768a85b569d14770dc47e40a57154f217d
stepan.rutz <stepan.rutz@gmx.de>
2026-05-13 11:43:11 +0200

    init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -15,4 +16,9 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
