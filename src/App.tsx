d010dde380695d135913b5fa62ecf1650403e069
stepan.rutz <stepan.rutz@gmx.de>
2026-05-19 11:44:51 +0200

    warmup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { MyButton } from "./MyButton";
-import { useQuote, type QuoteType } from "./useQuote";
+import { useQuote, useQuotesWarmup, type QuoteType } from "./useQuote";
 
 export function App() {
   const [ id, setId ] = useState(1)
+  useQuotesWarmup(1, 20)
+  const client = useQueryClient()
   return (
     <div
       className="w-screen h-screen flex 
-    flex-col items-center justify-center p-4 gap-2"
+    flex-col items-center justify-start p-4 gap-2"
     >
+      <div className="flex gap-2">
+        <MyButton onClick={() => setId(id-1)}>Prev</MyButton>
+        <MyButton onClick={() => setId(id+1)}>Next</MyButton>
+      </div>
       <QuoteDisplay id={id} />
-      <MyButton onClick={() => setId(id+1)}>Next</MyButton>
+      <QuoteDisplay id={3} />
+      <MyButton onClick={() => {
+        // post to server, then efresh
+        client.invalidateQueries({
+          queryKey: [ "quote" ]
+        }) 
+      }}>Reset</MyButton>
     </div>
   );
 }
@@ -21,8 +34,12 @@ function QuoteDisplay({ id }: { id: number}) {
 }
 
 function QuoteRenderer({ quote } : { quote: QuoteType }) {
+  console.log("render quote")
   return (
 <div className="p-4 m-2 shadow-xl bg-zinc-200 flex flex-col gap-2">
+    <div className="self-end text-gray-400 text-sm">
+      Gerendert am: {new Date().toLocaleTimeString()}
+    </div>
     <div>{quote.quote}</div>
     <div className="self-end text-gray-400 text-sm">
       {quote.author}
--- src/useQuote.ts
+++ src/useQuote.ts
@@ -1,15 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export type QuoteType = { id: number; quote: string; author: string }
 
 export function useQuote(id: number) {
     return useQuery({
         queryKey: [ "quote", id],
-        queryFn: async () => getQuote(id)
+        queryFn: async () => getQuote(id),
+        staleTime: 60 * 60 * 1_000,
+        // Always publish the newly fetched object so consumers rerender
+        // even when the server returns the same values.
+        structuralSharing: false,
     })
 }
 
 export async function getQuote(id: number) {
   const resp = await fetch("https://dummyjson.com/quotes/" + id)
   return await resp.json() as QuoteType
+}
+
+export function useQuotesWarmup(from: number, to: number) {
+    const client = useQueryClient()
+    for (let id = from; id <= to; id++) {
+        client.ensureQueryData({
+            queryFn: () => getQuote(id),
+            queryKey: [ "quote", id]
+        })
+    }
 }
