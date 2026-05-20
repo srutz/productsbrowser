853668b545f6070c613fe230a5e8cfb548dbc15d
stepan.rutz <stepan.rutz@gmx.de>
2026-05-20 10:50:22 +0200

    form components

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { NuqsAdapter } from "nuqs/adapters/react";
-import { StrictMode } from "react";
+import { StrictMode, type ComponentProps } from "react";
 import { createRoot } from "react-dom/client";
 import { LuZap } from "react-icons/lu";
 import { createBrowserRouter, NavLink, Outlet, RouterProvider } from "react-router";
 import { QuoteDisplay } from "./App";
 import "./index.css";
+import { cn } from "./lib/utils";
 
 const router = createBrowserRouter([{
   path: "/", element: <Root/>, children: [
     { path: "/", element: <Main/> },
     { path: "/quotes/:id", element: <QuoteDisplay/> },
     { path: "/quotes", element: <QuoteDisplay/> },
-    { path: "/about", element: <About/> }
+    { path: "/about", element: <About/> },
+    { path: "/form", element: <Form/> },
   ]
 }])
 
@@ -39,6 +41,7 @@ function Menubar() {
       <LuZap size={32}></LuZap>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/quotes">Quotes</NavLink>
+      <NavLink to="/form">Form</NavLink>
       <NavLink to="/about">About</NavLink>
     </div>)
 }
@@ -61,4 +64,40 @@ reactRoot.render(
     </NuqsAdapter>
   </StrictMode>,
 );
+
+
+function Form() {
+  return (<VBox className="gap-8">
+    <VBox>
+      <Label htmlFor="input1">Vorname</Label>
+      <Input aria-id="input1" placeholder="Vorname eingeben"></Input>
+    </VBox>
+    <VBox className="shadow-xl">
+      <Label htmlFor="input2">E-Mail</Label>
+      <Input id="input2" placeholder="E-Mail eingeben" type="email"></Input>
+    </VBox>
+  </VBox>)}
+
+function Label({ type, ...props }: ComponentProps<"label"> & { type?: "warning" | "error" }) {
+  return (<label {...props} 
+    className={cn( 
+      type === "error" && "font-bold text-red-700",
+      type === "warning" && "text-green-700",
+    )}/>)
+}
+
+function Input({ className, ...props }: ComponentProps<"input">) {
+  return (
+    <div className={cn("flex items-center gap-1", className)}>
+      <input className="py-2 px-2 border border-gray-200" {...props}></input>
+    </div>)
+}
+
+function VBox({ className, ...props }: ComponentProps<"div">) {
+  return (<div {...props} className={cn("flex flex-col gap-2", className)} />
+)}
+
+function HBox({ className, ...props }: ComponentProps<"div">) {
+  return (<div {...props} className={cn("flex items-center gap-2", className)} />
+)}
 
