d3996c0f3138621b03482a427d7abecdf9dc3337
stepan.rutz <stepan.rutz@gmx.de>
2026-06-30 08:21:12 +0200

    emptied main

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { NuqsAdapter } from "nuqs/adapters/react";
-import { StrictMode, type ComponentProps } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { LuZap } from "react-icons/lu";
 import { createBrowserRouter, NavLink, Outlet, RouterProvider } from "react-router";
-import { QuoteDisplay } from "./App";
 import "./index.css";
-import { cn } from "./lib/utils";
+
 
 const router = createBrowserRouter([{
   path: "/", element: <Root/>, children: [
     { path: "/", element: <Main/> },
-    { path: "/quotes/:id", element: <QuoteDisplay/> },
-    { path: "/quotes", element: <QuoteDisplay/> },
     { path: "/about", element: <About/> },
-    { path: "/form", element: <Form/> },
   ]
 }])
 
@@ -40,8 +36,6 @@ function Menubar() {
     <div className="flex gap-4 p-4 border-b border-gray-400 bg-zinc-100 items-center">
       <LuZap size={32}></LuZap>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/quotes">Quotes</NavLink>
-      <NavLink to="/form">Form</NavLink>
       <NavLink to="/about">About</NavLink>
     </div>)
 }
@@ -51,6 +45,7 @@ function Footer() {
     flex justify-end">Greetings from Köln</div>
 }
 
+const SHOW_REACT_QUERY_DEVTOOLS = !true;
 const client = new QueryClient();
 const rootElement = document.getElementById("root") as HTMLElement;
 const reactRoot = createRoot(rootElement);
@@ -60,44 +55,10 @@ reactRoot.render(
       <QueryClientProvider client={client}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-      <ReactQueryDevtools client={client}/>
+      {SHOW_REACT_QUERY_DEVTOOLS && <ReactQueryDevtools client={client}/>}
     </NuqsAdapter>
   </StrictMode>,
 );
 
 
-function Form() {
-  return (<VBox className="gap-8">
-    <VBox>
-      <Label htmlFor="input1">Vorname</Label>
-      <Input aria-id="input1" placeholder="Vorname eingeben"></Input>
-    </VBox>
-    <VBox className="shadow-xl">
-      <Label htmlFor="input2">E-Mail</Label>
-      <Input id="input2" placeholder="E-Mail eingeben" type="email"></Input>
-    </VBox>
-  </VBox>)}
-
-function Label({ type, ...props }: ComponentProps<"label"> & { type?: "warning" | "error" }) {
-  return (<label {...props} 
-    className={cn( 
-      type === "error" && "font-bold text-red-700",
-      type === "warning" && "text-green-700",
-    )}/>)
-}
-
-function Input({ className, ...props }: ComponentProps<"input">) {
-  return (
-    <div className={cn("flex items-center gap-1", className)}>
-      <input className="py-2 px-2 border border-gray-200" {...props}></input>
-    </div>)
-}
-
-function VBox({ className, ...props }: ComponentProps<"div">) {
-  return (<div {...props} className={cn("flex flex-col gap-2", className)} />
-)}
-
-function HBox({ className, ...props }: ComponentProps<"div">) {
-  return (<div {...props} className={cn("flex items-center gap-2", className)} />
-)}
 
